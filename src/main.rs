da200876df840e7bad2706e6db4c3b76eccbd620
nikkehtine <nikkehtine@int.pl>
2024-05-30 17:44:03 +0200

    Move tokenizer to a submodule

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,8 @@
-use std::{
-    env,
-    fmt::{Debug, Write},
-    fs,
-};
+use crate::tokenizer::tokenize;
+use hydro::{Token, TokenType};
+use std::{env, fmt::Write, fs};
 
-#[derive(Debug)]
-pub enum TokenType {
-    Exit,
-    IntLit,
-    Semi,
-}
-
-#[derive(Debug)]
-pub struct Token {
-    ttype: TokenType,
-    value: Option<String>,
-}
-
-pub fn tokenize(s: &str) -> Result<Vec<Token>, &'static str> {
-    let mut buffer = String::new();
-    let mut tokens: Vec<Token> = Vec::new();
-
-    let mut chars = s.chars();
-    while let Some(c) = chars.next() {
-        if c.is_alphabetic() || !buffer.is_empty() && c.is_alphanumeric() {
-            buffer.push(c);
-            continue;
-        } else if c.is_digit(10) {
-            buffer.push(c);
-            continue;
-        } else if c.is_whitespace() || c.is_ascii_punctuation() {
-            if buffer.as_str() == "exit" {
-                tokens.push(Token {
-                    ttype: TokenType::Exit,
-                    value: None,
-                });
-            } else if let Ok(_) = buffer.as_str().parse::<i32>() {
-                tokens.push(Token {
-                    ttype: TokenType::IntLit,
-                    value: Some(buffer.clone()),
-                });
-            } else if buffer.is_empty() {
-                continue;
-            } else {
-                return Err("You messed up");
-            }
-            buffer.clear();
-            if c == ';' {
-                tokens.push(Token {
-                    ttype: TokenType::Semi,
-                    value: None,
-                });
-            }
-        } else {
-            return Err("You messed up");
-        }
-    }
-
-    Ok(tokens)
-}
+mod tokenizer;
 
 pub fn assemble(tokens: &Vec<Token>) -> Result<String, &'static str> {
     let iter = tokens.iter().enumerate();
---
+++ src/tokenizer/mod.rs
@@ -0,0 +1,44 @@
+use crate::{Token, TokenType};
+
+pub fn tokenize(s: &str) -> Result<Vec<Token>, &'static str> {
+    let mut buffer = String::new();
+    let mut tokens: Vec<Token> = Vec::new();
+
+    let mut chars = s.chars();
+    while let Some(c) = chars.next() {
+        if c.is_alphabetic() || !buffer.is_empty() && c.is_alphanumeric() {
+            buffer.push(c);
+            continue;
+        } else if c.is_digit(10) {
+            buffer.push(c);
+            continue;
+        } else if c.is_whitespace() || c.is_ascii_punctuation() {
+            if buffer.as_str() == "exit" {
+                tokens.push(Token {
+                    ttype: TokenType::Exit,
+                    value: None,
+                });
+            } else if let Ok(_) = buffer.as_str().parse::<i32>() {
+                tokens.push(Token {
+                    ttype: TokenType::IntLit,
+                    value: Some(buffer.clone()),
+                });
+            } else if buffer.is_empty() {
+                continue;
+            } else {
+                return Err("You messed up");
+            }
+            buffer.clear();
+            if c == ';' {
+                tokens.push(Token {
+                    ttype: TokenType::Semi,
+                    value: None,
+                });
+            }
+        } else {
+            return Err("You messed up");
+        }
+    }
+
+    Ok(tokens)
+}
